a4165e071aff9c83e4b69f7b8b6c0e488e89fa57
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-05-08 00:54:17 +0300

    update

--- output.tf
+++ output.tf
@@ -1,14 +1,8 @@
-output "application_name" {
+output "argocd_application" {
   description = "Name of the Argo CD application"
-  value       = argocd_application.my_app.metadata[0].name
+  value       = argocd_application.httpd.metadata[0].name
 }
-
-output "application_repo_url" {
+output "argocd_repository" {
   description = "Git repository URL used by the application"
-  value       = argocd_application.my_app.spec[0].source[0].repo_url
-}
-
-output "application_destination_namespace" {
-  description = "Target namespace where the app is deployed"
-  value       = argocd_application.my_app.spec[0].destination[0].namespace
+  value       = argocd_application.httpd.spec[0].source[0].repo_url
 }
