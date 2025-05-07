dd284a0042c640ec3638170ea3945f8f0bd51271
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-05-08 00:33:00 +0300

    update

--- repository.tf
+++ repository.tf
@@ -1,5 +1,9 @@
-resource "argocd_repository_credentials" "gitlab" {
-  url      = "https://gitlab.com/virtapp-infra/global-charts.git"
-  username = "global"
-  password = "glpat-HgKuBDx7eN2SQ4vDbwWe"
+resource "argocd_repository" "gitlab-repo" {
+  repo           = "https://gitlab.com/virtapp-infra/global-charts.git"
+  type           = "git"
+  username       = var.git_username     # optional
+  password       = var.git_password     # optional
+  insecure       = false                # true if skipping TLS validation
+  enable_lfs     = false
+  project        = "default"            # optional
 }
