a0d1e182d816a1bf58e5a1ccae002cf857d64ca0
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-05-07 23:57:15 +0300

    update

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     argocd = {
-      source  = "oboukili/argocd"
-      version = ">= 4.0.0"
+      source  = "argoproj-labs/argocd"
+      version = ">= 6.0.0"
     }
   }
 }
 
 provider "argocd" {
-  server_addr = "argo-dashboard.virtapp.io:443"
+  server_addr = "argo-dashboard.virtapp.io"
   username    = "admin"
   password    = "admin"
   insecure    = true
